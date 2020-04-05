bed9d143be5c3ac73994dfd801b2a5cb90e28e4d
StarbugStone <pint2stone@gmail.com>
2020-04-05 16:11:30 +0200

    adding articles and images entities

---
+++ src/Entity/Article.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\ArticleImage", mappedBy="article")
+     */
+    private $articleImages;
+
+    public function __construct()
+    {
+        $this->articleImages = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|ArticleImage[]
+     */
+    public function getArticleImages(): Collection
+    {
+        return $this->articleImages;
+    }
+
+    public function addArticleImage(ArticleImage $articleImage): self
+    {
+        if (!$this->articleImages->contains($articleImage)) {
+            $this->articleImages[] = $articleImage;
+            $articleImage->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArticleImage(ArticleImage $articleImage): self
+    {
+        if ($this->articleImages->contains($articleImage)) {
+            $this->articleImages->removeElement($articleImage);
+            // set the owning side to null (unless already changed)
+            if ($articleImage->getArticle() === $this) {
+                $articleImage->setArticle(null);
+            }
+        }
+
+        return $this;
+    }
+}
