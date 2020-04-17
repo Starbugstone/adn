004bdd147e0c74361ea42f980ca9513442d7cd84
StarbugStone <pint2stone@gmail.com>
2020-04-17 06:51:35 +0200

    adding the slug to the article

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
@@ -47,6 +48,12 @@ class Article
      * @ORM\OneToMany(targetEntity="App\Entity\ArticleVideo", mappedBy="Article")
      */
     private $articleVideos;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Gedmo\Slug(fields={"title"})
+     */
+    private $slug;
 
     public function __construct()
     {
@@ -165,6 +172,18 @@ class Article
                 $articleVideo->setArticle(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
